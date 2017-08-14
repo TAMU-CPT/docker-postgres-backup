4663fc2c498256f76c8330744873e79501d168a6
E Rasche <hxr@hx42.org>
2017-08-14 14:42:15 +0200

    gzip data

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKUP_CMD="pg_dump -f /backup/\${BACKUP_NAME} ${EXTRA_OPTS}"
+BACKUP_CMD="pg_dump ${EXTRA_OPTS} | gzip > /backup/\${BACKUP_NAME}"
 
 echo "=> Creating backup script"
 rm -f /backup.sh
